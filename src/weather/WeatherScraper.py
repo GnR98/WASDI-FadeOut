adaeea6e112f633379a408dd0a2ca5c58fff29e4
GnR98 <101529585+GnR98@users.noreply.github.com>
2022-09-10 12:40:18 +0200

    Update WeatherScraper.py

--- src/weather/WeatherScraper.py
+++ src/weather/WeatherScraper.py
@@ -4,6 +4,9 @@ from meteostat import Point, Daily
 from datetime import timedelta
 import math
 import os
+# import module
+import json
+from geopy.geocoders import Nominatim
 
 
 
@@ -26,16 +29,20 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
         print("There is no data from the input file for the district of "+district)
         exit()
     # New columns used to insert the details of preciptations on each requested date before and after the repair date
+    # initialize Nominatim API
+    geolocator = Nominatim(user_agent="geoapiExercises")
+
     sheet["Dettaglio (in mm), "+daysBefore+" giorni prima (in ordine crescente di data)"]=" "
     sheet["Dettaglio (in mm), "+daysAfter+" giorni dopo (in ordine crescente di data)"]=" "
-
     # For each date in the excel, put the mean value of the precipitation
-    # calculated on the days before('YourDataInAList1') and after('YourDataInaAList2') the repair date
-    # and put on Data1 and Data2 the details of preciptations of each day (same convention as prior comment)
-    YourDataInAList1=[]
-    Data1=[]
-    YourDataInAList2 = []
-    Data2 = []
+    # calculated on the days before('yourDataInAList1') and after('YourDataInaAList2') the repair date
+    # and put on data1 and data2 the details of preciptations of each day (same convention as prior comment)
+    yourDataInAList1=[]
+    prcp1=[]
+    yourDataInAList2 = []
+    prcp2 = []
+    locNames=[]
+    cityVillageTown=[]
     for i, row in sheet.iterrows():
         print(row)
         print("\n")
@@ -45,7 +52,20 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
         location = Point(row[3], row[2])
         end1 = row[1]
         end2 = row[1] + timedelta(days=int(daysAfter))
-
+        if( not math.isnan(row[3]) and not math.isnan(row[2]) ):
+            geolocation = geolocator.reverse(str(row[3]) + "," + str(row[2]))
+            if ("city" in geolocation.raw["address"]):
+                cityVillageTown.append("city")
+                locNames.append(geolocation.raw["address"]["city"])
+            elif("town" in geolocation.raw["address"]):
+                cityVillageTown.append("town")
+                locNames.append(geolocation.raw["address"]["town"])
+            elif("village" in geolocation.raw["address"]):
+                cityVillageTown.append("village")
+                locNames.append(geolocation.raw["address"]["village"])
+        else:
+            locNames.append("nan")
+            cityVillageTown.append("nan")
 
         data1 = Daily(location, start1, end1)
         data2 = Daily(location, start2, end2)
@@ -59,7 +79,7 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
             tot = 0
             #Mean value of precipitation on the days before the repair date
             for q in data1.prcp:
-                Data1.append(q)
+                prcp1.append(q)
                 if not math.isnan(q):
                     tot += q
                 else:
@@ -69,10 +89,10 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
                 tot = "nan"
             else:
                 tot = tot / count
-            YourDataInAList1.append(tot)
+            yourDataInAList1.append(tot)
         else:
             tot = "nan"
-            YourDataInAList1.append("nan")
+            yourDataInAList1.append("nan")
 
         #Column used to store if there were or not any precipitations before the repair date
         #the row values for this column are determined from the mean value
@@ -90,7 +110,7 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
             tot = 0
             # Mean value of precipitation on the days after the repair date
             for q in data2.prcp:
-                Data2.append(q)
+                prcp2.append(q)
                 if not math.isnan(q):
                     tot += q
                 else:
@@ -100,10 +120,10 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
                 tot = "nan"
             else:
                 tot = tot / count
-            YourDataInAList2.append(tot)
+            yourDataInAList2.append(tot)
         else:
             tot = "nan"
-            YourDataInAList2.append("nan")
+            yourDataInAList2.append("nan")
 
         #Column used to store if there were or not any precipitations after the repair date
         #the row values for this column are determined from the mean value
@@ -116,15 +136,17 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
                 sheet.at[i, "Ha piovuto dopo i lavori ?"]="true"
 
         #Insertion of detailed values of precipitation (in mm)
-        sheet.at[i,"Dettaglio (in mm), "+ daysBefore + " giorni prima (in ordine crescente di data)"]=Data1.copy()
-        sheet.at[i,"Dettaglio (in mm), "+ daysAfter + " giorni dopo (in ordine crescente di data)"]=Data2.copy()
+        sheet.at[i,"Dettaglio (in mm), "+ daysBefore + " giorni prima (in ordine crescente di data)"]=prcp1.copy()
+        sheet.at[i,"Dettaglio (in mm), "+ daysAfter + " giorni dopo (in ordine crescente di data)"]=prcp2.copy()
 
-        Data1.clear()
-        Data2.clear()
+        prcp1.clear()
+        prcp2.clear()
 
     #Columns used to store the mean values
-    sheet.insert(6,"Media precipitazioni nei "+ daysBefore + " giorni prima dalla data di inizio(in mm)",value=YourDataInAList1)
-    sheet.insert(7,"Media precipitazioni nei "+ daysAfter + " giorni dopo la data di inizio(in mm)",value=YourDataInAList2)
+    sheet.insert(6,"Media precipitazioni nei "+ daysBefore + " giorni prima dalla data di inizio(in mm)",value=yourDataInAList1)
+    sheet.insert(7,"Media precipitazioni nei "+ daysAfter + " giorni dopo la data di inizio(in mm)",value=yourDataInAList2)
+    sheet.insert(8,"Type of location",value=cityVillageTown)
+    sheet.insert(9,"Dettaglio località",value=locNames)
 
     #Earliest date of observation
     xlsStartDate = sheet["Data Inizio Esito"].get(sheet["Data Inizio Esito"].index[0]).strftime("%d-%m-%Y")
@@ -139,21 +161,43 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
 
 if __name__ == '__main__':
     # File location
-    loc = os.getcwd()+"\\Geolocation.xlsx"
-    if(os.path.isfile(loc)):
+    file = open("config.json")
+    jsonData = json.load(file)
 
+    daysBefore=""
+    daysAfter=""
+    district=""
+    loc=""
+
+    if("DAYSBEFORE" in jsonData.keys()):
+        daysBefore=jsonData.get("DAYSBEFORE")
+    if(daysBefore==""):
         daysBefore = input("Please specify for how many days before the repair you want to fetch data :\n")
-        while(not daysBefore.isnumeric()):
+        while (not daysBefore.isnumeric()):
             print("Please insert a number\n")
             daysBefore = input("Please specify for how many days before the repair you want to fetch data :\n")
 
+
+    if("DAYSAFTER" in jsonData.keys()):
+        daysAfter= jsonData.get("DAYSAFTER")
+    if(daysAfter==""):
         daysAfter = input("Please specify  for how many days after the repair you want to fetch data :\n")
         while (not daysAfter.isnumeric()):
             print("Please insert a number")
-            daysAfter = input("Please specify for how many days after the repair you want to fetch data :\n")
+            daysAfter = str(input("Please specify for how many days after the repair you want to fetch data :\n"))
 
+    if("DISTRICT" in jsonData.keys()):
+        district = jsonData.get("DISTRICT").upper()
+    if(district==""):
         district = input("Please specify the district :\n").upper()
-        WeatherStat(loc,daysBefore,daysAfter,district)
 
+    if("EXCELLOC" in jsonData.keys()):
+       loc= jsonData.get("EXCELLOC")
+    if(loc==""):
+        loc= input("Please specify location of excel file in input :\n")
+
+
+    if(os.path.isfile(loc)):
+        WeatherStat(loc,str(daysBefore),str(daysAfter),district)
     else:
-        print("ERROR: No file named \"Geolocation.xlsx\" on project directory, check if the file is in the project directory")
+        print("ERROR: Excel source file not found")
