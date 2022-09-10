85a6815ce841430a909b678bed1341da88b66201
M-AIC <93790615+M-AIC@users.noreply.github.com>
2022-09-10 15:53:23 +0200

    Translation WeatherScraper.py
    Translated comments and the column names. Might need to be updated as the original source file had Italian column namees

--- src/weather/WeatherScraper.py
+++ src/weather/WeatherScraper.py
@@ -32,17 +32,21 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
     # initialize Nominatim API
     geolocator = Nominatim(user_agent="geoapiExercises")
 
-    sheet["Dettaglio (in mm), "+daysBefore+" giorni prima (in ordine crescente di data)"]=" "
-    sheet["Dettaglio (in mm), "+daysAfter+" giorni dopo (in ordine crescente di data)"]=" "
+    sheet["Precipitations (in mm), from "+daysBefore+" days before (in ascending order of date)"]=" "
+    sheet["Precipitations (in mm), until "+daysAfter+" days after (in ascending order of date)"]=" "
     # For each date in the excel, put the mean value of the precipitation
     # calculated on the days before('yourDataInAList1') and after('YourDataInaAList2') the repair date
-    # and put on data1 and data2 the details of preciptations of each day (same convention as prior comment)
+    # and put on prcp1 and prcp2 the details of preciptations of each day (same convention as prior comment)
     yourDataInAList1=[]
     prcp1=[]
     yourDataInAList2 = []
     prcp2 = []
+    
+    #For each row the column cityVillageTown is used to store a value that determines if the given coordinates belong to a city, a town or a village
+    #The locNames column contains the name of the city, town or village
     locNames=[]
     cityVillageTown=[]
+    
     for i, row in sheet.iterrows():
         print(row)
         print("\n")
@@ -52,6 +56,9 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
         location = Point(row[3], row[2])
         end1 = row[1]
         end2 = row[1] + timedelta(days=int(daysAfter))
+        
+        #If the coordinates are not NAN the geolocaation process starts then, depending on the result, the columns
+        #cityVillageTown and locNames are filled with the correct values
         if( not math.isnan(row[3]) and not math.isnan(row[2]) ):
             geolocation = geolocator.reverse(str(row[3]) + "," + str(row[2]))
             if ("city" in geolocation.raw["address"]):
@@ -66,7 +73,8 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
         else:
             locNames.append("nan")
             cityVillageTown.append("nan")
-
+            
+        #Using meteostat API
         data1 = Daily(location, start1, end1)
         data2 = Daily(location, start2, end2)
         data1 = data1.fetch()
@@ -98,11 +106,11 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
         #the row values for this column are determined from the mean value
         match tot:
             case 0.0:
-                sheet.at[i, "Ha piovuto prima dei lavori ?"]="false"
+                sheet.at[i, "Did it rain before the repair date ?"]="false"
             case "nan":
-                sheet.at[i, "Ha piovuto prima dei lavori ?"]="nan"
+                sheet.at[i, "Did it rain before the repair date ?"]="nan"
             case _:
-                sheet.at[i, "Ha piovuto prima dei lavori ?"]="true"
+                sheet.at[i, "Did it rain before the repair date ?"]="true"
 
         count= data2.prcp.size;
 
@@ -129,24 +137,24 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
         #the row values for this column are determined from the mean value
         match tot:
             case 0.0:
-                sheet.at[i, "Ha piovuto dopo i lavori ?"]="false"
+                sheet.at[i, "Did it rain after the repair date ?"]="false"
             case "nan":
-                sheet.at[i, "Ha piovuto dopo i lavori ?"]="nan"
+                sheet.at[i, "Did it rain after the repair date ?"]="nan"
             case _:
-                sheet.at[i, "Ha piovuto dopo i lavori ?"]="true"
+                sheet.at[i, "Did it rain after the repair date ?"]="true"
 
         #Insertion of detailed values of precipitation (in mm)
-        sheet.at[i,"Dettaglio (in mm), "+ daysBefore + " giorni prima (in ordine crescente di data)"]=prcp1.copy()
-        sheet.at[i,"Dettaglio (in mm), "+ daysAfter + " giorni dopo (in ordine crescente di data)"]=prcp2.copy()
+        sheet.at[i,"Precipitations (in mm), from "+daysBefore+" days before (in ascending order of date)"]=prcp1.copy()
+        sheet.at[i,"Precipitations (in mm), until "+daysAfter+" days after (in ascending order of date)"]=prcp2.copy()
 
         prcp1.clear()
         prcp2.clear()
 
     #Columns used to store the mean values
-    sheet.insert(6,"Media precipitazioni nei "+ daysBefore + " giorni prima dalla data di inizio(in mm)",value=yourDataInAList1)
-    sheet.insert(7,"Media precipitazioni nei "+ daysAfter + " giorni dopo la data di inizio(in mm)",value=yourDataInAList2)
+    sheet.insert(6,"Mean precipitation value in the "+ daysBefore + " days before the repair date (in mm)",value=yourDataInAList1)
+    sheet.insert(7,"Mean precipitation value in the "+ daysAfter + " days after the repair date (in mm)",value=yourDataInAList2)
     sheet.insert(8,"Type of location",value=cityVillageTown)
-    sheet.insert(9,"Dettaglio località",value=locNames)
+    sheet.insert(9,"Name of location",value=locNames)
 
     #Earliest date of observation
     xlsStartDate = sheet["Data Inizio Esito"].get(sheet["Data Inizio Esito"].index[0]).strftime("%d-%m-%Y")
@@ -160,7 +168,7 @@ def WeatherStat(loc,daysBefore,daysAfter,district):
 
 
 if __name__ == '__main__':
-    # File location
+    # JSON File location
     file = open("config.json")
     jsonData = json.load(file)
 
@@ -168,7 +176,9 @@ if __name__ == '__main__':
     daysAfter=""
     district=""
     loc=""
-
+    
+    #if a key does not exists or the value of the key is not given (causing variable == ""), a console input will be requested
+    #in order to fill the required variable
     if("DAYSBEFORE" in jsonData.keys()):
         daysBefore=jsonData.get("DAYSBEFORE")
     if(daysBefore==""):
@@ -196,7 +206,7 @@ if __name__ == '__main__':
     if(loc==""):
         loc= input("Please specify location of excel file in input :\n")
 
-
+    #When every required variable is filled the script checks whether the source excle file exists before calling the function
     if(os.path.isfile(loc)):
         WeatherStat(loc,str(daysBefore),str(daysAfter),district)
     else:
