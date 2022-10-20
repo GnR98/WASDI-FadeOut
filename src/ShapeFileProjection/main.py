2e44c16289002649ada147c3ea1c4e0197719837
M-AIC <93790615+M-AIC@users.noreply.github.com>
2022-10-20 12:24:54 +0200

    Update main.py

--- src/ShapeFileProjection/main.py
+++ src/ShapeFileProjection/main.py
@@ -7,7 +7,8 @@ import json
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
 from pyproj import Geod
-import  fiona
+from pyproj import Transformer
+import fiona
 from collections import OrderedDict
 from shapely.geometry import shape
 from shapely.geometry import LineString,Point
@@ -24,7 +25,19 @@ class Projection():
         self.dict = OrderedDict()
 
 
-    def run(self,excelloc,shapeloc,district):
+    def getAllProjections(self, excelloc, shapeloc, district):
+        """
+        Filter the Excel file for all the
+        Create a new Excel file called "NewGeolocation_ShapefileProj_"+district+".xlsx" with all the columns inside
+        NewGeolocation plus two more containing the coordinates of the projected points
+
+        :param self:
+        :param pipesVector: Array containing all the candidate pipes on which we try the projection
+        :param interventionRow: Row formatted like the ones inside  the NewGeolocation file and thus containing
+                                 all the information of the intervention
+        :return:
+        """
+
         wb = pd.read_excel(excelloc, na_values=['NA'])
         self.sheet = wb[wb['Comune'].str.contains(district)]
         self.sheet = self.sheet[self.sheet['Intervento'].str.contains('rete')]
@@ -52,7 +65,7 @@ class Projection():
                         if (row[2] in j['properties']['STREET'].upper() or j['properties']['STREET'].upper() in row[2]):
                             temp.append(j)
             if(temp):
-                self.proiezione(temp,row)
+                self.projectOnClosestPipe(temp, row)
 
 
 
@@ -69,29 +82,103 @@ class Projection():
                         self.sheet.at[i, "Proiezione (LNG)"] = self.dict.get(j).y
 
         self.sheet.to_excel(
-            "Provalo4.xlsx",
-            index=False);
+            "NewGeolocation_ShapefileProj_"+district+".xlsx",index=False)
 
 
-    def proiezione(self,vettoreTubature,lavorazione):
-        #finalTubatura = None
+    def projectOnClosestPipe(self, pipesVector, interventionRow):
+
+        """
+        Project the point inside the interventionRow on all the pipes inside the pipesVector in order to find
+        the closest one, then fill the dict attribute with key = intervention address and value = closest coordinates
+
+        :param self:
+        :param pipesVector: Array containing all the candidate pipes on which we try the projection
+        :param interventionRow: Row formatted like the ones inside  the NewGeolocation file and thus containing
+                                all the information of the intervention
+        :return:
+        """
+        #finalPipe = None
         minDistance = 1000000
         i=0
         tempPoint=None
-        for tubatura in vettoreTubature:
+        for tubatura in pipesVector:
             for point in tubatura["geometry"]["coordinates"]:
                 if(i==1):
                     line = LineString([tempPoint,point])
-                    if(self.geod.geometry_length(LineString(nearest_points(line,Point(lavorazione[9],lavorazione[8]))))<minDistance):
-                        minDistance=self.geod.geometry_length(LineString(nearest_points(line,Point(lavorazione[9],lavorazione[8]))))
-                        if(lavorazione[3]==lavorazione[3]):
-                            self.dict[lavorazione[2]+":"+lavorazione[3]]=nearest_points(line,Point(lavorazione[9],lavorazione[8]))[0]
+                    if(self.geod.geometry_length(LineString(nearest_points(line, Point(interventionRow[9], interventionRow[8]))))<minDistance):
+                        minDistance=self.geod.geometry_length(LineString(nearest_points(line, Point(interventionRow[9], interventionRow[8]))))
+                        if(interventionRow[3]==interventionRow[3]):
+                            self.dict[interventionRow[2] + ":" + interventionRow[3]]=nearest_points(line, Point(interventionRow[9], interventionRow[8]))[0]
                         else:
-                            self.dict[lavorazione[2]] = nearest_points(line, Point(lavorazione[9], lavorazione[8]))[0]
+                            self.dict[interventionRow[2]] = nearest_points(line, Point(interventionRow[9], interventionRow[8]))[0]
                     i=0
                 else:
                     tempPoint=point
                     i+=1
+
+    def convertShapefile(self,shapeloc):
+
+        """
+        Convert and substitute the coordinates in the selected shapefile from EPSG 32632 to WGS84.
+        \b
+        --THIS WILL OVERWRITE THE ORIGINAL COORDINATES--
+        \b
+        After a successful use of this function the conversion will not be needed anymore for the selected shapefile
+
+        :param self:
+        :param shapeloc: Absolute location of the shapefile that needs to be converted
+        :return:
+        """
+
+        shape = fiona.open(shapeloc)
+        # first feature of the shapefile
+        transformer = Transformer.from_crs("EPSG:32632", "EPSG:4326")
+        shapeDict = OrderedDict()
+        # conversione coordinate da epsg 32632 a wgs84 e correzione vie delle tubature
+        for i in shape:
+            temp = []
+            shapeDict[i["id"]] = i
+            for j in i["geometry"]["coordinates"]:
+                temp.append(transformer.transform(j[0], j[1]))
+            # i["geometry"]["coordinates"] = temp
+            shapeDict[i["id"]]["geometry"]["coordinates"] = temp
+            # inserisco la via corretta nello shape file controllando le coordinate
+            if ("road" in self.do_reverse(temp[0]).raw['address']) :
+                shapeDict[i["id"]]['properties']['STREET'] = self.do_reverse(temp[0]).raw['address']['road']
+
+
+        my_schema = {'properties': OrderedDict(
+            [('OBJECTID', 'int:10'), ('NAME_NUM', 'str:254'), ('MUN', 'str:254'), ('STREET', 'str:254'),
+             ('ISTAT', 'str:6'), ('MUN_OWN', 'str:254'), ('MAIN_FUNCT', 'str:254'), ('SPEC_FUNCT', 'str:254'),
+             ('HYDR_FUNCT', 'str:254'), ('WAT_QUAL', 'str:254'), ('OPERATOR', 'str:254'), ('STATUS', 'str:254'),
+             ('DETERMINAT', 'str:254'), ('DIAMETER', 'str:254'), ('MATERIAL', 'str:254'), ('MATERIAL_T', 'str:254'),
+             ('LENGTH', 'float:31.15'), ('GROUND_ELE', 'float:31.15'), ('COVERING', 'float:31.15'),
+             ('SURF_POS', 'str:254'), ('PIPE_NAME', 'str:254'), ('START_NODE', 'str:254'), ('END_NODE', 'str:254'),
+             ('DATE_ACQ', 'date'), ('DATE_INS', 'date'), ('REMARK', 'str:254'), ('CREA_DATE', 'date'),
+             ('LA_ED_DATE', 'date'), ('DMA', 'str:254')]), 'geometry': 'LineString'}
+
+        with fiona.open(shapeloc, 'w', driver='ESRI Shapefile', schema=my_schema) as output:
+            for key, value in shapeDict.items():
+                output.write({'geometry': value["geometry"], 'properties': value["properties"]})
+
+    def do_reverse(self, coordinate, attempt=1, max_attempts=5):
+
+        """
+        Try the reverse geocoding operation at most a number of times equal to max_attempts.
+        It will return the same object that the reverse method of the Nominatim API returns
+
+        :param self:
+        :param coordinate: coordinates of the place you want to execute the reverse geocoding
+        :param attempt: current attempt (you don't need to specify it)
+        :param max_attempts: maximum number of attempts to try the operation
+        :return:
+        """
+        try:
+            return self.geolocator.reverse(coordinate)
+        except GeocoderTimedOut:
+            if attempt <= max_attempts:
+                return self.do_reverse(coordinate, attempt=attempt + 1)
+            raise
 
 
 # Press the green button in the gutter to run the script.
@@ -119,8 +206,13 @@ if __name__ == '__main__':
 
     if (os.path.isfile(excelloc)):
         if(os.path.isfile(shapeloc)):
+
             proj= Projection()
-            proj.run(excelloc,shapeloc,district)
+
+            # If the shapefile has not been corrected yet uncomment the line below (correction is needed only once for each shapefile)
+            # proj.convertShapefile(shapeloc)
+
+            proj.getAllProjections(excelloc, shapeloc, district)
         else:
             print("ERROR: Shape source file not found")
     else:
